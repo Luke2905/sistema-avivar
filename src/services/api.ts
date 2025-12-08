673ac698e103caba73a15938d54f8da80d5e3f6e
Luke2905 <laranjeiralucas17@gmail.com>
2025-12-08 14:09:28 -0300

    atualização link api

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api', // O endereço do nosso Back-end
+    baseURL: 'https://api-avivar.onrender.com/api', // O endereço do nosso Back-end
 });
 
 // Interceptador: Toda vez que a gente chamar a API, ele já coloca o Token automaticamente
